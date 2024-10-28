58a4152cd11a83afa8e9ab4ee03cffbfa8ba6a8e
karlobasic <karlo.basic@fer.unizg.hr>
2024-10-28 17:35:21 +0100

    Add files via upload
    Pocetna definicija baze

---
+++ IzvorniKod/Baze/definicija.sql
@@ -0,0 +1,29 @@
+CREATE TABLE KORISNIK
+(
+  idKor SERIAL,
+  email VARCHAR(320) NOT NULL,
+  PRIMARY KEY (idKor),
+  UNIQUE (email)
+);
+
+CREATE TABLE OGLAS
+(
+  idOgl SERIAL,
+  vrsDog VARCHAR(50) NOT NULL,
+  nazDog VARCHAR(50) NOT NULL,
+  mjesto VARCHAR(50) NOT NULL,
+  datum DATE NOT NULL,
+  brSje INT,
+  vrsUla VARCHAR(50),
+  status BOOLEAN DEFAULT FALSE NOT NULL, /*false znaci da jos nije prodan */
+  cijena INT NOT NULL,
+  kupujeidKor INT,
+  prodajeidKor INT NOT NULL,
+  PRIMARY KEY (idOgl),
+  FOREIGN KEY (kupujeidKor) REFERENCES KORISNIK(idKor),
+  FOREIGN KEY (prodajeidKor) REFERENCES KORISNIK(idKor),
+  CONSTRAINT istiKupacProdavac CHECK (kupujeidKor != prodajeidKor)
+);
+
+
+
