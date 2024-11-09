fa3d3a0d38b0786c55b9891ae6e871d089bd06bb
laurabarisic <laura.barisic@fer.unizg.hr>
2024-11-09 18:25:31 +0100

    Upcomingevents, Profile, TicketDetails,, Create

--- IzvorniKod/Frontend/tickex/src/pages/UpcomingEvents.jsx
+++ IzvorniKod/Frontend/tickex/src/pages/UpcomingEvents.jsx
@@ -1,40 +1,44 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import useFetch from '../components/useFetch';
 import '../style/UpcomingEvents.css';
 
-function UpcomingEvents(){
+function UpcomingEvents() {
     const [Tickets, setTickets] = useState([]);
     const { data: tickets, isPending: isTicketsPending, error: ticketsError } = useFetch("http://localhost:8080/api/tickets");
 
     useEffect(() => {
-        if (tickets){
+        if (tickets) {
             setTickets(tickets);
         }
     }, [tickets]);
 
-    return(
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-CA'); 
+    };
+
+    return (
         <>
-        <div className="events-container">
-            <h1>Nadolazeći događaji</h1>
-            <div className="cards-container-box">
-                {
-                    Tickets.map((ticket) => {
-                        return(
-                            <button key={ticket.id} className="moreInfoBtn">
-                                <div className="card">
-                                    <h1>{ticket.eventName}</h1>
-                                    <h4>{ticket.location}</h4>
-                                    <h2>{ticket.eventDate}</h2>
-                                </div>
-                            </button>
-                        )
-                    })
-                }
+            <div className="events-container">
+                <h1>Nadolazeći događaji</h1>
+                <div className="cards-container-box">
+                    {
+                        Tickets.map((ticket) => {
+                            return (
+                                <button key={ticket.id} className="moreInfoBtn">
+                                    <div className="card">
+                                        <h1>{ticket.eventName}</h1>
+                                        <h4>{ticket.location}</h4>
+                                        <h2>{formatDate(ticket.eventDate)}</h2>
+                                    </div>
+                                </button>
+                            )
+                        })
+                    }
+                </div>
             </div>
-        </div>
         </>
-    )
+    );
 }
 
 export default UpcomingEvents;
