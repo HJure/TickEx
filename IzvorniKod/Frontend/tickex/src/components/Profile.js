dbd0a08314487c4fc1429857114dbc4405dd0b17
laurabarisic <laura.barisic@fer.unizg.hr>
2024-11-10 20:20:07 +0100

    Profile.js i Upcomingevents.js

--- IzvorniKod/Frontend/tickex/src/components/Profile.js
+++ IzvorniKod/Frontend/tickex/src/components/Profile.js
@@ -3,16 +3,17 @@ import { useNavigate } from 'react-router-dom';
 import useFetch from './useFetch';
 import axios from 'axios'; 
 import TicketList from './TicketList'; 
-import Trash from './Trash';
 import '../style/profile.css';
 import { Link } from "react-router-dom";
 
 function Profile() {
     const [profile, setProfile] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [userID, setUserID] = useState(null);
     const navigate = useNavigate();
+    const access_token = localStorage.getItem("access_token");
 
     useEffect(() => {
-        const access_token = localStorage.getItem("access_token");
         if (access_token) {
             axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${access_token}`, {
                 headers: {
@@ -21,51 +22,110 @@ function Profile() {
                 }
             }).then((res) => {
                 setProfile(res.data);
-                console.log(res.data);
+                setEmail(res.data.email);
             }).catch((err) => console.log(err));
         }
-    }, []);
+    }, [access_token]);
+
+    useEffect(() => {
+        const fetchUserID = async () => {
+            if (email) {
+                try {
+                    const response = await fetch(`http://localhost:8080/api/users/getId?email=${email}`, {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': `Bearer ${access_token}`,
+                            'Content-Type': 'application/json',
+                        },
+                        credentials: 'include',
+                    });
+            
+                    if (!response.ok) {
+                        throw new Error('Error while fetching user ID');
+                    }
+            
+                    const data = await response.json();
+                    setUserID(data);
+                    localStorage.setItem("userID", data); 
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+            }
+        };
+
+        fetchUserID();
+    }, [email, access_token]);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            if (userID) {
+                try {
+                    const response = await fetch(`http://localhost:8080/api/users/${userID}`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${access_token}`,
+                        },
+                        credentials: 'include',
+                    });
+            
+                    if (!response.ok) {
+                        throw new Error('Error while fetching user data');
+                    }
+            
+                    const data = await response.json();
+                    localStorage.setItem("user_email", data.email); 
+                    localStorage.setItem("user_first_name", data.imeKor); 
+                    localStorage.setItem("user_last_name", data.prezimeKor); 
+                    localStorage.setItem("user_registration_date", data.datumUla);  
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+            }
+        };
+
+        fetchUserData();
+    }, [userID, access_token]);
 
     const logOut = () => {
         localStorage.removeItem("access_token");
+        localStorage.removeItem("userID");
+        localStorage.removeItem("user_email");
+        localStorage.removeItem("user_first_name");
+        localStorage.removeItem("user_last_name");
+        localStorage.removeItem("user_registration_date");
+
         setProfile(null);
         navigate('/signup');
     };
 
     const { data: tickets, isPending: isTicketsPending, error: ticketsError } = useFetch("http://localhost:8080/api/tickets");
-    const { data: trashes, isPending: isTrashesPending, error: trashesError } = useFetch("http://localhost:5000/trash");
+    const filteredTickets = tickets ? tickets.filter(ticket => ticket.owner.id === parseInt(userID)) : [];
 
     return profile ? (
         <div className='profilediv'>
             <div className="profile-container">
                 <img src={profile.picture} alt="user" className="profile-image" />
-                <h3 className="profile-heading">Korisnik prijavljen</h3>
-                <p className="profile-info">Ime: {profile.name}</p>
-                <p className="profile-info">Email Adresa: {profile.email}</p>
-                <button onClick={logOut} className="logout-button">Odjavi se</button>
+                <h3 className="profile-heading">User Logged In</h3>
+                <p className="profile-info">Name: {profile.name}</p>
+                <p className="profile-info">Email Address: {profile.email}</p>
+                <button onClick={logOut} className="logout-button">Log out</button>
             </div>
             <div>
                 {ticketsError && <div className='error'>{ticketsError}</div>}
-                {isTicketsPending && <div className='loading'>Učitavam karte...</div>}
+                {isTicketsPending && <div className='loading'>Loading tickets...</div>}
                 <div className="my_offers_trash_container">
                     <div className="my_offers">
-                        {ticketsError && <div className="error">{ticketsError}</div>}
-                        {isTicketsPending && <div className="loading">Učitavam karte...</div>}
-                        {tickets && <TicketList tickets={tickets} title="My offers:" />}
-                    </div>
-                    <div className="trash">
-                        {trashesError && <div className="error">{trashesError}</div>}
-                        {isTrashesPending && <div className="loading">Učitavam otpad...</div>}
-                        {trashes && <Trash trashes={trashes} title="My trash:" />}
+                        {tickets && <TicketList tickets={filteredTickets} title="My offers:" />}
                     </div>
                 </div>
             </div>
-            <br/>
             <br />
-            <Link to="/create" className="newBlog">Dodaj novi blog</Link>
+            <br />
+            <Link to="/create" className="newBlog">Add New Blog</Link>
         </div>
     ) : (
-        <p className="loading-text">Učitavam profil...</p>
+        <p className="loading-text">Loading profile...</p>
     );
 }
 
--- IzvorniKod/Frontend/tickex/src/pages/UpcomingEvents.jsx
+++ IzvorniKod/Frontend/tickex/src/pages/UpcomingEvents.jsx
@@ -5,15 +5,23 @@ import '../style/UpcomingEvents.css';
 function UpcomingEvents() {
     const [Tickets, setTickets] = useState([]);
     const { data: tickets, isPending: isTicketsPending, error: ticketsError } = useFetch("http://localhost:8080/api/tickets");
+    
+    useEffect(() => {
+        if (tickets) {
+            const userID = localStorage.getItem("userID");
 
-    useEffect(() => {if (tickets) {
-        setTickets(tickets);
-    }
-}, [tickets]);
+            if (userID) {
+                const filteredTickets = tickets.filter(ticket => ticket.owner.id !== parseInt(userID));
+                setTickets(filteredTickets);
+            } else {
+                setTickets(tickets);
+            }
+        }
+    }, [tickets]);
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-        return date.toLocaleDateString('en-CA'); 
+        return date.toLocaleDateString('en-CA');
     };
 
     return (
@@ -28,7 +36,6 @@ function UpcomingEvents() {
                                     <div className="card">
                                         <h1>{ticket.eventName}</h1>
                                         <h4>{ticket.location}</h4>
-                                        <h4>{ticket.eventTypeId.nazVrDog}</h4>
                                         <h2>{formatDate(ticket.eventDate)}</h2>
                                     </div>
                                 </button>
