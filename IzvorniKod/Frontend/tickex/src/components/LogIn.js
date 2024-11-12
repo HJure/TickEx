77cac49d4277129af4ba773f2fd5eba8b2ec0401
Karlo Bašić <karlo.basic@fer.unizg.hr>
2024-11-12 21:42:42 +0100

    Update LogIn.js

--- IzvorniKod/Frontend/tickex/src/components/LogIn.js
+++ IzvorniKod/Frontend/tickex/src/components/LogIn.js
@@ -5,7 +5,7 @@ import '../style/login.css';
 function Login() {
 
     const handleLoginClick = () => {
-        window.location.href = `https://backend-3qyr.onrender.com/oauth2/authorization/google`;
+        window.location.href = `https://backend-3qyr.onrender.com/api/oauth2/authorization/google`;
     };
 
     return (
