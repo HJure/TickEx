efdc46b1a2901ba3e21cc7bfb8c24396615d9b0d
Karlo Bašić <karlo.basic@fer.unizg.hr>
2024-11-14 01:22:16 +0100

    Update SecurityConfig.java

--- IzvorniKod/progi-project/src/main/java/progi_project/config/SecurityConfig.java
+++ IzvorniKod/progi-project/src/main/java/progi_project/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(withDefaults()).csrf().disable()
-            .authorizeHttpRequests(auth -> auth.requestMatchers("/", "/error", "/api/users/register", "api/tickets/**", "/api/users/**").permitAll()
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/", "/error", "/api/users/register", "api/tickets/**", "/api/users/**", "/api/vrsta-dogadaja").permitAll()
                                     .anyRequest().authenticated()
                                     ).oauth2Login()
                                     .successHandler(customOAuth2SuccessHandler)
