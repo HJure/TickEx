c363bc4ea5b855708895c07a8db622e842ae56de
David Kovčo <david.kovco@fer.unizg.hr>
2024-12-18 23:07:02 +0100

    Spajanje na vremensku prognozu
    Iskoristio sam daily limit tako da treba provjeriti kada se vrati, ali bi trebalo raditi

--- IzvorniKod/Frontend/tickex/src/components/TicketDetails.js
+++ IzvorniKod/Frontend/tickex/src/components/TicketDetails.js
@@ -13,7 +13,8 @@ const TicketDetails = ({ url }) => {
     const navigate = useNavigate();
     const [canDelete, setCanDelete] = useState(false);
     const [canBringBack, setBringBack] = useState(false);
-    console.log(ticket)
+    const [weatherData, setWeatherData] = useState({})
+    const [weatherLocation, setWeatherLocation] = useState('')
 
     // State for like button image
     const [likeImage, setLikeImage] = useState("../images/unlike.png");
@@ -24,7 +25,34 @@ const TicketDetails = ({ url }) => {
             setCanDelete(ticket.owner.id === parseInt(userID) && ["u prodaji", "aukcija", "razmjena"].includes(ticket.isExchangeAvailable));
             setBringBack(ticket.owner.id === parseInt(userID) && ticket.isExchangeAvailable === "obrisano");
         }
+
+        if(ticket){
+            setWeatherLocation(ticket.location);
+        }
     }, [ticket]);
+
+    const api_key = 'JYLAZZ9VPACY4FFAAYWSEDJDJ'
+    const searchWeather = async () => {
+        const urlWeather = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${weatherLocation}?unitGroup=metric&include=days&key=${api_key}&contentType=json`
+        const resWeather = await fetch(urlWeather)
+        const searchWeatherData = await resWeather.json()
+        setWeatherData(searchWeatherData)
+    };
+
+    useEffect(() => {
+        if (weatherLocation) {
+            searchWeather()
+        }
+    }, [weatherLocation]);
+
+    const getDaysDifference = (ticketDate) => {
+        const today = new Date();
+        const ticketDateObj = new Date(ticketDate);
+        const diffTime = ticketDateObj - today;
+        const diffDays = diffTime / (1000 * 3600 * 24);
+  
+        return diffDays;
+    };
 
     const handleDelete = () => {
         const deleteUrl = `${url}/${id}/status`.replace(/([^:]\/)\/+/g, "$1"); 
@@ -103,7 +131,7 @@ const TicketDetails = ({ url }) => {
                             className="like" 
                             src={likeImage} 
                             alt="like" 
-                            onClick={handleLikeClick} // Add click event handler here
+                            onClick={handleLikeClick}
                         />
                     </h2>
 
@@ -116,8 +144,17 @@ const TicketDetails = ({ url }) => {
                             <span> Status:</span> <span className="answer">{ticket.isExchangeAvailable}</span> |
                             <span> Cijena:</span> <span className="answer">{ticket.price} €</span> |
                             <span> Vrsta događaja:</span> <span className="answer">{ticket.eventTypeId.nazVrDog}</span> |
-                            <span> Broj sjedala:</span> <span className="answer">{ticket.seatNumber !== null ? ticket.seatNumber : "-"}</span>
-                            <span> Izbrisana:</span> <span className="answer">{ticket.obrisanoTime !== null ? ticket.obrisanoTime : "-"}</span>
+                            <span> Broj sjedala:</span> <span className="answer">{ticket.seatNumber !== null ? ticket.seatNumber : "-"}</span> |
+                            <span> Izbrisana:</span> <span className="answer">{ticket.obrisanoTime !== null ? ticket.obrisanoTime : "-"}</span> |
+                            <span>
+                                Vrijeme: 
+                            </span>
+                            <span className="answer">
+                                {
+                                    weatherData?.days?
+                                        (getDaysDifference(ticket.eventDate) >= 0 && getDaysDifference(ticket.eventDate) < 15 ? weatherData?.days?.[Math.ceil(getDaysDifference(ticket.eventDate))]?.conditions || "-" : "-") : "-"
+                                }
+                            </span>
                         </p>
                         <br/>
                         <p className="ticket-posted-by">Objavio: {ticket.owner.imeKor} {ticket.owner.prezimeKor}</p>
