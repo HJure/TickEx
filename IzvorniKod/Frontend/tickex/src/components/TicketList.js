82de7ffa4056dcfdb81abf99f1efb0cc6b6dd722
irmazecevic <irma.zecevic@fer.unizg.hr>
2025-01-12 15:46:53 +0100

    Izgled karata u kartici Moje Ponude
    - manje css promjene

--- IzvorniKod/Frontend/tickex/src/components/TicketList.js
+++ IzvorniKod/Frontend/tickex/src/components/TicketList.js
@@ -6,12 +6,18 @@ const TicketList = ({ tickets, title }) => {
 
     return (  
         <div className="ticket-list">
-            <h2>{ title }</h2>
             <div className="tickets">
                 {tickets.map((ticket) => (
                     <div className="ticket-preview" key={ticket.id}>   
                         <Link to={ `/tickets/${ticket.id}` }>
+                            <div className='eventType'>
+                            <h1>{ ticket.isExchangeAvailable }</h1>
+                            </div>
+                            <div className='eventInfo'>
                             <h2>{ ticket.eventName }</h2>
+                            <h3>{ticket.eventDate}</h3>
+                            <h3>{ticket.location}</h3>
+                            </div>
                         </Link>
                     </div>
                 ))}
