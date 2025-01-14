dff27a8c500f1fa1a340f884859ce2e5d4c7ae9e
laurabarisic <laura.barisic@fer.unizg.hr>
2025-01-14 02:40:08 +0100

    Više info linkovi

--- IzvorniKod/Frontend/tickex/src/components/Exchange.js
+++ IzvorniKod/Frontend/tickex/src/components/Exchange.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import "../style/Exchange.css";
+import { Link } from 'react-router-dom';
 
 const Exchange = () => {
   const [results, setResult] = useState([]);
@@ -18,58 +19,60 @@ const Exchange = () => {
       <ul style={{ listStyle: "none", padding: 0 }}>
         {filteredResults.map((exchange) => (
           <li key={exchange.id}>
-            <div className="ponudena">
-              <h3>Ponuđena karta</h3>
-              <p>
-                <strong>Naziv događaja:</strong> {exchange.eventName}
-              </p>
-              <p>
-                <strong>Datum događaja:</strong> {exchange.eventDate}
-              </p>
-              <p>
-                <strong>Lokacija:</strong> {exchange.location}
-              </p>
-              {exchange.seatNumber && (
+            <div className="cont">
+              <div className="ponudena">
+                <h3>Ponuđena karta</h3>
                 <p>
-                  <strong>Broj sjedala:</strong> {exchange.seatNumber}
+                  <strong>Naziv događaja:</strong> {exchange.eventName}
                 </p>
-              )}
-              {exchange.ticketType && (
                 <p>
-                  <strong>Tip karte:</strong> {exchange.ticketType}
+                  <strong>Datum događaja:</strong> {exchange.eventDate}
                 </p>
-              )}
-              <p>
-                <strong>Vlasnik:</strong> {exchange.owner?.imeKor} {exchange.owner?.prezimeKor}
-              </p>
+                <p>
+                  <strong>Lokacija:</strong> {exchange.location}
+                </p>
+                {exchange.seatNumber && (
+                  <p>
+                    <strong>Broj sjedala:</strong> {exchange.seatNumber}
+                  </p>
+                )}
+                {exchange.ticketType && (
+                  <p>
+                    <strong>Tip karte:</strong> {exchange.ticketType}
+                  </p>
+                )}
+                <p>
+                  <strong>Vlasnik:</strong> {exchange.owner?.imeKor} {exchange.owner?.prezimeKor}
+                </p>
+              </div>
+              <img src="/images/up-arrow.png" alt="Arrow" />
+              <div className="trazeno">
+                <h3>Tražena karta</h3>
+                <p>
+                  <strong>Naziv događaja:</strong> {exchange.wantedEventName}
+                </p>
+                <p>
+                  <strong>Datum događaja:</strong> {exchange.wantedDate}
+                </p>
+                <p>
+                  <strong>Lokacija:</strong> {exchange.wantedLocation}
+                </p>
+                {exchange.wantedSeatNumber && (
+                  <p>
+                    <strong>Broj sjedala:</strong> {exchange.wantedSeatNumber}
+                  </p>
+                )}
+                {exchange.wantedTicketType && (
+                  <p>
+                    <strong>Tip karte:</strong> {exchange.wantedTicketType}
+                  </p>
+                )}
+              </div>
             </div>
-            <img src="/images/up-arrow.png" alt="Arrow" />
-            <div className="trazeno">
-              <h3>Tražena karta</h3>
-              <p>
-                <strong>Naziv događaja:</strong> {exchange.wantedEventName}
-              </p>
-              <p>
-                <strong>Datum događaja:</strong> {exchange.wantedDate}
-              </p>
-              <p>
-                <strong>Lokacija:</strong> {exchange.wantedLocation}
-              </p>
-              {exchange.wantedSeatNumber && (
-                <p>
-                  <strong>Broj sjedala:</strong> {exchange.wantedSeatNumber}
-                </p>
-              )}
-              {exchange.wantedTicketType && (
-                <p>
-                  <strong>Tip karte:</strong> {exchange.wantedTicketType}
-                </p>
-              )}
-            </div>
+            <Link to={`/tickets/${exchange.id}`} className="more-info">Više info</Link>
           </li>
         ))}
       </ul>
-
     </div>
   );
 };
