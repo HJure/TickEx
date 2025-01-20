ac5b7857877ca006a31d76805ec44f007e12e5d6
David Kovčo <david.kovco@fer.unizg.hr>
2025-01-20 20:05:25 +0100

    Prebacivanje Id usera u deaktivira tablicu

---
+++ IzvorniKod/progi-project/src/main/java/progi_project/controller/DeaktiviraController.java
@@ -0,0 +1,42 @@
+package progi_project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import progi_project.model.Deaktivira;
+import progi_project.model.DeaktiviraRequest;
+import progi_project.service.DeaktiviraService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/deaktivira")
+public class DeaktiviraController {
+
+    @Autowired
+    private DeaktiviraService deaktiviraService;
+
+    @PostMapping
+    public ResponseEntity<String> createDeaktivira(@RequestBody DeaktiviraRequest request) {
+        try {
+            deaktiviraService.createDeaktivira(request.getDeaktiviraIdKor(), request.getDeaktiviranIdKor());
+            return ResponseEntity.ok("Deactivation record created successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/user-deactivated")
+    public ResponseEntity<List<Deaktivira>> getDeactivationsForUser(@RequestParam int deaktiviranIdKor) {
+        List<Deaktivira> deactivations = deaktiviraService.getDeactivationsForUser(deaktiviranIdKor);
+        return ResponseEntity.ok(deactivations);
+    }
+
+    @GetMapping("/user-deactivator")
+    public ResponseEntity<List<Deaktivira>> getDeactivationsByUser(@RequestParam int deaktiviraIdKor) {
+        List<Deaktivira> deactivations = deaktiviraService.getDeactivationsByUser(deaktiviraIdKor);
+        return ResponseEntity.ok(deactivations);
+    }
+}
+
