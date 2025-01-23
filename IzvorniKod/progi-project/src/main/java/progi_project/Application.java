faec289479b6d631637c703e77af0a364eefbcd3
Karlo Bašić <karlo.basic@fer.unizg.hr>
2025-01-23 12:16:21 +0100

    Update Application.java

--- IzvorniKod/progi-project/src/main/java/progi_project/Application.java
+++ IzvorniKod/progi-project/src/main/java/progi_project/Application.java
@@ -12,6 +12,9 @@ public class Application {
 		System.out.println("DB_URL: " + System.getenv("DB_URL"));
         	System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
         	System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
+		System.out.println("FRONTEND_URL: " + System.getenv("FRONTEND_URL"));
+		System.out.println("BACKEND_URL: " + System.getenv("BACKEND_URL"));
+		System.out.println("password: " + System.getenv("password"));
 		SpringApplication.run(Application.class, args);
 	}
 
