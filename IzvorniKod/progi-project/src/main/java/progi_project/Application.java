e061491fdb75359e0f0003efe5a7984be17e1ffe
Karlo Bašić <karlo.basic@fer.unizg.hr>
2025-01-23 11:56:33 +0100

    Provjera env varijabli

--- IzvorniKod/progi-project/src/main/java/progi_project/Application.java
+++ IzvorniKod/progi-project/src/main/java/progi_project/Application.java
@@ -9,6 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("DB_URL: " + System.getenv("DB_URL"));
+        	System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+        	System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
 		SpringApplication.run(Application.class, args);
 	}
 
