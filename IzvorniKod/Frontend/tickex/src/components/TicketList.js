53065389824ac9191e23cd7abc516b655b72919e
irmazecevic <irma.zecevic@fer.unizg.hr>
2025-01-23 23:50:22 +0100

    .css fix

--- IzvorniKod/Frontend/tickex/src/components/TicketList.js
+++ IzvorniKod/Frontend/tickex/src/components/TicketList.js
@@ -11,11 +11,11 @@ const TicketList = ({ tickets, title }) => {
                     <div className="ticket-preview" key={ticket.id}>   
                         <Link to={ `/tickets/${ticket.id}` }>
                             <div className='eventType'>
-                                <h1>{ ticket.isExchangeAvailable }</h1>
+                                <h1>{(ticket.owner.id === parseInt(localStorage.getItem("user_ID")))? "kupljeno" : ticket.isExchangeAvailable }</h1>
                             </div>
                             <div className='eventInfo'>
                                 <h2>{ ticket.eventName }</h2>
-                                {ticket.endPrice && <h3>End Price: {ticket.endPrice}</h3>}
+                                {ticket.endPrice && <h3>Konačna cijena: {ticket.endPrice}</h3>}
                                 <h3>{ticket.eventDate}</h3>
                                 <h3>{ticket.location}</h3>
                             </div>
