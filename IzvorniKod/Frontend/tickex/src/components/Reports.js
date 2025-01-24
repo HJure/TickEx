d6e2e2fc13d8e650aaa1cd9a8b22e1b3d084cf4f
laurabarisic <laura.barisic@fer.unizg.hr>
2025-01-24 23:06:24 +0100

    <br />

--- IzvorniKod/Frontend/tickex/src/components/Reports.js
+++ IzvorniKod/Frontend/tickex/src/components/Reports.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../style/Reports.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/Reports.css";
 
 const Reports = () => {
     const [reports, setReports] = useState([]);
     const [error, setError] = useState(null);
-    const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
-    const access_token = localStorage.getItem('access_token');
-    const Admin = localStorage.getItem('user_admin') === 'true';
+    const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+    const access_token = localStorage.getItem("access_token");
+    const Admin = localStorage.getItem("user_admin") === "true";
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchReports = async () => {
             try {
                 const response = await fetch(`${backendUrl}/api/reports/dashboard`, {
-                    method: 'GET',
+                    method: "GET",
                     headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${access_token}`
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${access_token}`,
                     },
-                    credentials: 'include'
+                    credentials: "include",
                 });
 
-                if (!response.ok) { 
-                    throw new Error('Access denied!');
+                if (!response.ok) {
+                    throw new Error("Access denied!");
                 }
 
                 const data = await response.json();
                 setReports(data);
             } catch (err) {
                 setError(err.message);
-                console.error('Error fetching reports:', err);
+                console.error("Error fetching reports:", err);
             }
         };
 
@@ -41,8 +41,8 @@ const Reports = () => {
         return <div>{error}</div>;
     }
 
-    if(!Admin){
-        return <div>Nemate admin ovlasti!</div>
+    if (!Admin) {
+        return <div>Nemate admin ovlasti!</div>;
     }
 
     const handleUserClick = (userId) => {
@@ -51,46 +51,48 @@ const Reports = () => {
 
     return (
         <div>
-        <h2 className="reports-title">Prijave</h2>
-        {reports.length > 0 ? (
-            <div>
-                {reports.map((report) => (
-                    <div key={`${report.reporter.id}-${report.reported.id}`} className="report-card">
-                        <div className="report-card-header">
-                            <button 
-                                onClick={() => handleUserClick(report.reporter.id)} 
-                                className="link-button">
-                                <strong>Prijavio:</strong> {report.reporter.imeKor} {report.reporter.prezimeKor} ({report.reporter.email})
-                                <br>
-                                <strong>Datum ulaska:</strong>{report.reported.datumUla}
-                                </br>
-                                <br>
-                                <strong>Ocjena:</strong>{report.reported.ocjena}
-                                </br> 
-                            </button>
-                        
-                            <button 
-                                onClick={() => handleUserClick(report.reported.id)} 
-                                className="link-button">
-                                <strong>Prijavljen:</strong> {report.reported.imeKor} {report.reported.prezimeKor} ({report.reported.email}) 
-                                <br>
-                                <strong>Datum ulaska:</strong>{report.reported.datumUla}
-                                </br>
-                                <br>
-                                <strong>Ocjena:</strong>{report.reported.ocjena}
-                                </br> 
-                            </button>
+            <h2 className="reports-title">Prijave</h2>
+            {reports.length > 0 ? (
+                <div>
+                    {reports.map((report) => (
+                        <div key={`${report.reporter.id}-${report.reported.id}`} className="report-card">
+                            <div className="report-card-header">
+                                <button
+                                    onClick={() => handleUserClick(report.reporter.id)}
+                                    className="link-button"
+                                >
+                                    <strong>Prijavio:</strong> {report.reporter.imeKor} {report.reporter.prezimeKor} (
+                                    {report.reporter.email})
+                                    <br />
+                                    <strong>Datum ulaska:</strong> {report.reported.datumUla}
+                                    <br />
+                                    <strong>Ocjena:</strong> {parseFloat(report.reported.ocjena)}
+                                    <br />
+                                </button>
+
+                                <button
+                                    onClick={() => handleUserClick(report.reported.id)}
+                                    className="link-button"
+                                >
+                                    <strong>Prijavljen:</strong> {report.reported.imeKor} {report.reported.prezimeKor} (
+                                    {report.reported.email})
+                                    <br />
+                                    <strong>Datum ulaska:</strong> {report.reported.datumUla}
+                                    <br />
+                                    <strong>Ocjena:</strong> {parseFloat(report.reported.ocjena)}
+                                    <br />
+                                </button>
+                            </div>
+                            <div className="strong">
+                                <strong>Razlog:</strong> {report.reason}
+                            </div>
                         </div>
-                        <div className='strong'>
-                            <strong>Razlog:</strong> {report.reason}
-                        </div>
-                    </div>
-                ))}
-            </div>
-        ) : (
-            <p className="no-reports">Nema prijava.</p>
-        )}
-    </div>
+                    ))}
+                </div>
+            ) : (
+                <p className="no-reports">Nema prijava.</p>
+            )}
+        </div>
     );
 };
 
